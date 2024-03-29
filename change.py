e6f6391cf54d61fed80caf2c8dc5d1b174198489
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-03-29 20:21:04 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,8 @@
 def change():
     expense = 23.75
     money = 100
+    print ("vuelto")
+    print ("pesos")
+    print (money - expense) // 1)
+    print ("centavos?")
+    print ((money-expense)%1)
