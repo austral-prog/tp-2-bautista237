2c5e9a0f6a1906e4faaff8a2d2071c8d36e5cf25
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-03-30 16:28:51 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,5 +2,5 @@ def change():
     expense = 23.75
     money = 100
     print(f"Ingresar gasto:\n{expense}")
-    print(f"Dinero rwcibid\n{money}")
+    print(f"Dinero recibid\n{money}")
     print (f'Vuelto\n\nPesos:\n{(money - expense) // 1}\nCentavos:\n{(money-expense)%1}')
