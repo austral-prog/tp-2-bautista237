2b269e13fcb9019ba013fb13e608e35c4c90d967
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-03-30 19:20:54 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,9 +1,5 @@
 def earth():
     X = "Bangladesh"
     Y = "Barbados"
-    resultado_X_antes_Y = cmp(X, Y) < 0
-    resultado_Y_antes_X = cmp(Y, X) < 0
-    print(f"El resultado de {X} viene primero en el diccionario que {Y} es {resultado_X_antes_Y}")
-    print(f"El resultado de {Y} viene primero en el diccionario que {X} es {resultado_Y_antes_X}")
-    earth()
-    
+    print(f"El resultado de {X} viene primero en el diccionario que {Y} es {X < Y}")
+    print(f"El resultado de {Y} viene primero en el diccionario que {X} es {Y < X}")
