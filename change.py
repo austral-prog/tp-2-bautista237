3686e8a68b0abc44dd5b9cca7512304b62c62086
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-03-30 18:16:01 -0300

    Update change.py

--- change.py
+++ change.py
@@ -14,6 +14,6 @@ def change():
     print(pesos_entero)
     print("Centavos:")
     Centavos = money - expense
-    Centavos_entero = int(vCentavos)
+    Centavos_entero = int(Centavos)
     Centavos_entero == 25
     print(Centavos_entero)
