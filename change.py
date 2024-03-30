4618744d1e372ea1159f4294b505e2e78ef4fab8
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-03-30 00:48:49 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,8 +1,5 @@
-def change():
-    expense = 23.75
-    money = 100
-    print ("Vuelto")
-    print ("Pesos")
-    print ((money - expense) // 1)
-    print ("Centavos")
-    print ((money-expense)%1)
+def earth():
+  x = "Bangladesh"
+  y = "Barbados"
+  print(f"The result of X comes first in the dictionary than Y is {x < y}")
+  print(f"The result of Y comes first in the dictionary than X is {y < x}")
